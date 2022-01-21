7a4d2201e1f76515d722a1e0c10260e8691dce9d
Luiz Souza <luiz.souz404@gmail.com>
2022-01-21 16:32:12 -0300

    feat: server and documentation

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
+
+import { handleErrors } from './middlewares/handleError';
+
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.use(router);
+app.use(handleErrors);
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
